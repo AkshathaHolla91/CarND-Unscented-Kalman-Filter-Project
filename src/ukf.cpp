f9e821318a64ce1f0a7b2f70e5d50ce1aeb3dd3a
Akshathaholla91 <akshathaholla91@gmail.com>
2018-03-26 19:31:49 +0530

    Small change

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -114,7 +114,7 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
     is_initialized_=true;
     return;
   }
-  long delta_t=(meas_package.timestamp_- time_us_)/1000000.0;
+  float delta_t=(meas_package.timestamp_- time_us_)/1000000.0;
   time_us_=meas_package.timestamp_;
 
 
