2357feca919ae1c1e49fe3d3f214b629209f4fc6
Akshathaholla91 <akshathaholla91@gmail.com>
2018-03-26 14:46:20 +0530

    Initial commit

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,8 +11,24 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+ VectorXd rmse(4);
+  rmse << 0,0,0,0;
+	
+	//Validate that the estimation vector size is greater than zero and that the ground truth  vector size is equal to the estimation vector size
+  if(estimations.size() != ground_truth.size() || estimations.size()==0 ){
+    cout << "Invalid data provided"<< endl;
+    return rmse;
+  }
+  //accumulate the squared errors
+  for(unsigned int i=0; i<estimations.size();i++){
+    VectorXd error= estimations[i]-ground_truth[i];
+		//co-efficientwise multiplication
+    error= error.array() * error.array();
+    rmse+=error;
+  }
+	//Calculate the mean
+  rmse=rmse/estimations.size();
+	//Calculate the square root
+  rmse=rmse.array().sqrt();
+  return rmse;
 }
--- src/ukf.cpp
+++ src/ukf.cpp
@@ -12,6 +12,7 @@ using std::vector;
  * This is scaffolding, do not modify
  */
 UKF::UKF() {
+  is_initialized_ = false;
   // if this is false, laser measurements will be ignored (except during init)
   use_laser_ = true;
 
@@ -25,10 +26,10 @@ UKF::UKF() {
   P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+  std_a_ = 0.2;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
+  std_yawdd_ = 0.2;
   
   //DO NOT MODIFY measurement noise values below these are provided by the sensor manufacturer.
   // Laser measurement noise standard deviation position1 in m
@@ -54,6 +55,16 @@ UKF::UKF() {
 
   Hint: one or more values initialized above might be wildly off...
   */
+  //set state dimension
+  n_x_= 5;
+  // Set augmented dimension
+  n_aug_ = 7;
+  lambda_ = 3 - n_aug_;
+  
+  Xsig_pred_ = MatrixXd(n_x_, 2 * n_aug_ + 1);
+  weights_ = VectorXd(2*n_aug_+1);
+
+
 }
 
 UKF::~UKF() {}
@@ -69,6 +80,53 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   Complete this function! Make sure you switch between lidar and radar
   measurements.
   */
+  if(!is_initialized_){
+    if(meas_package.sensor_type_==MeasurementPackage::RADAR){
+      float rho=meas_package.raw_measurements_(0);
+      float phi=meas_package.raw_measurements_(1);
+      float rho_dot=meas_package.raw_measurements_(2);
+
+      float px=rho*cos(phi);
+      float py=rho*sin(phi);
+
+      x_<<px, py,rho_dot, 0, 0;
+      P_<< 1, 0, 0, 0, 0,
+          0, 1, 0, 0, 0,
+          0, 0, 100, 0, 0,
+          0, 0, 0, 10000, 0,
+          0, 0, 0, 0, 10000;
+
+
+    }
+    else if(meas_package.sensor_type_==MeasurementPackage::LASER){
+      float px=meas_package.raw_measurements_(0);
+      float py=meas_package.raw_measurements_(1);
+      x_<<px, py, 5 , 0, 0;
+      P_<< 1, 0, 0, 0, 0,
+          0, 1, 0, 0, 0,
+          0, 0, 10000, 0, 0,
+          0, 0, 0, 10000, 0,
+          0, 0, 0, 0, 10000;
+
+    }
+    time_us_=meas_package.timestamp_;
+    is_initialized_=true;
+    return;
+  }
+  long delta_t=(meas_package.timestamp_- time_us_)/1000000.0;
+  time_us_=meas_package.timestamp_;
+
+
+  Prediction(delta_t);
+
+  if(meas_package.sensor_type_==MeasurementPackage::RADAR){
+    UpdateRadar(meas_package);
+  }
+  else if(meas_package.sensor_type_==MeasurementPackage::LASER){
+    UpdateLidar(meas_package);
+  }
+
+
 }
 
 /**
@@ -83,6 +141,62 @@ void UKF::Prediction(double delta_t) {
   Complete this function! Estimate the object's location. Modify the state
   vector, x_. Predict sigma points, the state, and the state covariance matrix.
   */
+  VectorXd x_aug=VectorXd(7);
+  MatrixXd P_aug=MatrixXd(7, 7);
+  x_aug.head(5)=x_;
+  x_aug(5)=0;
+  x_aug(6)=0;
+
+  P_aug.fill(0);
+  P_aug.topLeftCorner(5, 5)=P_;
+  P_aug(5,5)= std_a_*std_a_;
+  P_aug(6,6)= std_yawdd_* std_yawdd_;
+  MatrixXd Xsig_aug = MatrixXd(n_aug, 2 * n_aug + 1)
+  MatrixXd Root=P_aug.llt().matrixL();
+  // Augumented sigma values are generated
+  Xsig_aug.col(0)=x_aug;
+  for(int i=0;i<n_aug_;i++){
+    Xsig_aug.col(i+1)=x_aug+sqrt(lambda_+n_aug_)*Root.col(i);
+    Xsig_aug.col(i+1+n_aug_)=x_aug-sqrt(lambda_+n_aug_)*Root.col(i);
+  }
+
+  //predict sigma points
+  for(int i=0;i<2*n_aug_+1;i++){
+    double px=Xsig_aug(0,i);
+    double py=Xsig_aug(1,i);
+    double v=Xsig_aug(2,i);
+    double yaw=Xsig_aug(3,i);
+    double yawd=Xsig_aug(4,i);
+    double nu_a=Xsig_aug(5,i);
+    double nu_yawdd=Xsig_aug(6,i);
+
+    double px_pred, py_pred;
+    if(fabs(yawd)>0.001){
+      px_pred=px+v/yawd* (sin (yaw+yawd*delta_t)- sin(yaw));
+      py_pred = py + v/yawd * ( cos(yaw) - cos(yaw+yawd*delta_t) );
+    }
+    else{
+      px_pred=px+v*delta_t*cos(yaw);
+      py_pred=py+v*delta_t*sin(yaw);
+    }
+    double v_pred=v;
+    double yaw_pred=yaw+yawd*delta_t;
+    double yawd_pred=yawd;
+
+    px_pred+=0.5*nu_a*delta_t*delta_t*cos(yaw);
+    py_pred+=0.5*nu_a*delta_t*delta_t*sin(yaw);
+    v_pred+=nu_a*delta_t;
+    yaw_pred+=0.5*nu_yawdd*delta_t*delta_t;
+    yawd_pred+=nu_yawdd*delta_t;
+
+    Xsig_pred_(0,i)=px_pred;
+    Xsig_pred_(1,i)=py_pred;
+    Xsig_pred_(2,i)=v_pred;
+    Xsig_pred_(3,i)=yaw_pred;
+    Xsig_pred_(4,i)=yawd_pred;
+
+  }
+
 }
 
 /**
